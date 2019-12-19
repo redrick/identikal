25c44214eac24fe2bb784efb4cd1a160ff6aec7b
Andrej Antas <andrej@antas.cz>
2019-12-19 11:55:34 +0100

    Fix `FrozenError` on compare_method: :text

--- lib/identikal/version.rb
+++ lib/identikal/version.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Identikal
   VERSION = '0.1.2'
 end
--- spec/identikal_spec.rb
+++ spec/identikal_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe Identikal do
   it 'has a version number' do
     expect(Identikal::VERSION).not_to be nil
